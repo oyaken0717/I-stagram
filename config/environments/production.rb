97ce9d9acf25072c99060d61b02b07d76a73c35c
小山田賢司 <oyamadakenji@oyamadakenjinoMacBook-ea.local>
2019-03-05 15:37:29 +0900

    herokuアップ3/5 15:37

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: "young-hollows-25824.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "infinite-fortress-85880.herokuapp.com" }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
